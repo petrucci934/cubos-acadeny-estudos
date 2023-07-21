6a8483de5fee2bda38ac60a9b98f7a08d91fd433
petrucci <petrucci.lucenaplc@gmail.com>
2023-07-21 09:17:13 -0400

    Realizada refatoraçãoo de exercicios condicionais modulo 1

--- 10/index.js
+++ 10/index.js
@@ -1,11 +1,7 @@
 function calculaRaizes(ax, bx, c) {
-  let a = ax;
-  let b = bx;
-  let c1 = c;
-
-  //   const bascara = ax ** 2 + bx + c1;
-  const delta = b ** 2 - 4 * (a * c1);
+  const delta = bx ** 2 - 4 * (ax * c);
 
   return console.log(`Delta é igual a ${delta}`);
 }
 calculaRaizes(5, 5, 1);
+
