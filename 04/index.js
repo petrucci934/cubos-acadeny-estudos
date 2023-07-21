45cb1a8bb989fddd593b4938df9cbb64c44ee2c3
petrucci <petrucci.lucenaplc@gmail.com>
2023-07-21 09:12:07 -0400

    Realizada refatoraçãoo de exercicios condicionais modulo 1

--- 04/index.js
+++ 04/index.js
@@ -1,9 +1,8 @@
 function calculaMontante(capital, periodo) {
-  let c = capital ;
-  let t = periodo ;
   const I = 0.125; // Taxa fixa
-  const M = (c * (1 + I) ** t).toFixed(0);
+  const M = (capital * (1 + I) ** periodo).toFixed(0);
 
   return console.log(M);
 }
 calculaMontante(1000, 5);
+
