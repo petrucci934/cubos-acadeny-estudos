3c902ed1e9c03eedc1da8ca1673b62ad83d44b2b
petrucci <petrucci.lucenaplc@gmail.com>
2023-07-21 09:12:51 -0400

    Realizada refatoraçãoo de exercicios condicionais modulo 1

--- 05/index.js
+++ 05/index.js
@@ -1,7 +1,8 @@
 function calculaDistancia(x2, x1, y2, y1) {
-  let x = x2 - x1;
-  let y = y2 - y1;
-  let res = Math.sqrt(x ** 2 + y ** 2);
+  let pontox = x2 - x1;
+  let pontoy = y2 - y1;
+  let res = Math.sqrt(pontox** 2 + pontoy ** 2);
   return console.log(res);
 }
 calculaDistancia(1, 1, 4, 1);
+
