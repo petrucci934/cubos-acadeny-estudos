ef0d933b60ecdcea079443dd378d95f40ea9e413
petrucci <petrucci.lucenaplc@gmail.com>
2023-07-21 09:18:54 -0400

    Realizada refatoraçãoo de exercicios condicionais modulo 1

--- 12/index.js
+++ 12/index.js
@@ -1,10 +1,11 @@
 function calclulaTaxaJuros(montante, capital, parcelas) {
-  let m = montante;
-  let c = capital;
-  let n = parcelas;
-  let taxa = Math.pow(m / c, 1 / n) - 1;
+
+
+  let taxa = Math.pow(montante / capital, 1 / parcelas) - 1;
+  
   let res = (taxa * 100).toFixed(3);
 
   return console.log(`A taxa a ser paga é de: ${res}% ao mês`);
 }
 calclulaTaxaJuros(90000, 60000, 24);
+
