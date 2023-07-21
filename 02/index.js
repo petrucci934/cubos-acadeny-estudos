559f60885a232571750253c55dc48447d2c3c819
petrucci <petrucci.lucenaplc@gmail.com>
2023-07-21 09:08:53 -0400

    Realizada refatoraçãoo de exercicios condicionais modulo 1

--- 02/index.js
+++ 02/index.js
@@ -1,6 +1,7 @@
 function converteGraus(farenheigth) {
-  let f = farenheigth;
-  const CONVERTE = ((f - 32) * (5 / 9)).toFixed(0);
+  const CONVERTE = ((farenheigth- 32) * (5 / 9)).toFixed(0);
+
   return console.log(`${CONVERTE}°C`);
 }
 converteGraus(73);
+
