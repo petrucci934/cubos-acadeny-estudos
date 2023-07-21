a8ed46b2273b21b8074dfbedd0dd897de3da1721
petrucci <petrucci.lucenaplc@gmail.com>
2023-07-21 09:20:10 -0400

    Realizada refatoraçãoo de exercicios condicionais modulo 1

---
+++ 13/index.js
@@ -0,0 +1,6 @@
+function calculaAreaCilindro(raio, altura) {
+    const At = 2  * raio * (raio + altura);
+    return console.log(`A área total é de: ${At}pi`)
+  }
+  calculaAreaCilindro(3, 1);
+  
